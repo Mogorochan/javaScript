89b56edd2d59985fcfdba0b99e3fef53100f258a
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-09-14 17:53:58 -0500

    more changes

--- introduccion.js
+++ introduccion.js
@@ -1,2 +1,20 @@
 //JS maneja tipado dinámico, es decir, él mismo detecta qué tipo de dato es
 
+//unarios ++ incremento, -- decremento
+
+var edadX, edadY
+
+edadX = 34;
+edadY = 28;
+
+console.log(++ edadY);
+//Asignacion +=, -=, *=, /=, %= módulo
+
+var a = 11;
+var b = 5;
+
+var c = a % 5; //Residuo o resto de una division
+console.log(c); 
+
+a += b;
+console.log(a);
