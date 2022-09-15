cff7d459430deca2cffeb477c04716b6d9123cad
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-09-15 12:04:33 -0500

    primitives values definition

--- introduccion.js
+++ introduccion.js
@@ -1,5 +1,8 @@
-//JS maneja tipado dinámico, es decir, él mismo detecta qué tipo de dato es
-
+/*JS maneja tipado dinámico, es decir, él mismo detecta qué tipo de dato es. Sin embargo 
+también tiene algo llamado como primitive value, básicamente, son datos que no son un objeto y no tienen método,
+Actualmente son 6: undefined, string, number, bigint, boolean y symbol. 
+Null: es un tipo primitivo especial, que tiene un uso adicional para su valor: si el objeto no se hereda, se muestra null;
+*/
 //unarios ++ incremento, -- decremento
 
 var edadX, edadY
